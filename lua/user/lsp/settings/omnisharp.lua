e3c9f3db7b745b2e99271cf8dd6826d223132be9
Mathieu Scolas <worming@hotmail.be>
2022-12-02 22:18:11 +0100

    try csharp

---
+++ lua/user/lsp/settings/omnisharp.lua
@@ -0,0 +1,10 @@
+return {
+	settings = {
+		-- doesn't seems to work
+		enable_import_completion = true,
+		enable_roslyn_analyzers = true,
+		enable_analyzer_support = true,
+		enable_decompilation_support = true,
+		enableDecompilationSupport = true,
+	},
+}
