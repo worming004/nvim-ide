45247b21394ddf1c99542ceac70fdee99a2f1c36
Mathieu Scolas <mathieu.scolas@forem.be>
2023-03-06 11:00:36 +0100

    add yaml conf

---
+++ lua/user/lsp/settings/yamlls.lua
@@ -0,0 +1,5 @@
+local config = {}
+config.settings = {}
+config.settings['yaml.schemas'] = {}
+config.settings['yaml.schemas']['https://github.com/Microsoft/azure-pipelines-vscode/blob/main/service-schema.json'] = "/*"
+return config
