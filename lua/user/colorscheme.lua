dcbead0b672dc22c879418d71d7153cd9393df22
worming <worming@hotmail.be>
2023-03-31 16:56:31 +0200

    better colorscheme for signature active param

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,3 +1,10 @@
+require("tokyonight").setup({
+  -- see colors here https://github.com/folke/tokyonight.nvim/blob/184377a2c240b6a90fa9087e8ade6f5289f926d3/lua/tokyonight/colors.lua#L18
+  on_highlights = function(hl, colors)
+    hl.LspSignatureActiveParameter = { bg = colors.dark3 }
+  end
+})
+
 local colorscheme = "tokyonight-night"
 
 local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
