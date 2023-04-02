beaab6e4a8f5bdb4a5c16d0eebcddadc86ab713c
Mathieu Scolas <worming@hotmail.be>
2023-04-02 15:52:14 +0200

    add which-key

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -139,6 +139,16 @@ return packer.startup(function(use)
   use { "nvim-treesitter/nvim-treesitter-context" }
 
   use "b0o/schemastore.nvim"
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 1000
+      require("which-key").setup {
+      }
+    end
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
