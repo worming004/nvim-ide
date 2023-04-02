f390f9700e5d011fef25f748dbda85bca20f24e5
Mathieu Scolas <worming@hotmail.be>
2023-04-02 14:31:47 +0200

    add nvim tree context

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -116,6 +116,8 @@ return packer.startup(function(use)
   use { "j-hui/fidget.nvim" }
 
   use { "f-person/git-blame.nvim" }
+
+  use { "nvim-treesitter/nvim-treesitter-context"}
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
