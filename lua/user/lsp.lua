5a2fdc6a8780899b0960c9f0a3ddc53aecff9003
Mathieu Scolas <worming@hotmail.be>
2023-05-11 08:21:47 +0200

    autoformat

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -45,6 +45,9 @@ function M.config()
 
   local lspconfig = require "lspconfig"
 
+  -- https://www.reddit.com/r/neovim/comments/y9qv1w/autoformatting_on_save_with_vimlspbufformat_and/
+  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
   local on_attach = function(client, bufnr)
     if client.name == "tsserver" then
       client.server_capabilities.documentFormattingProvider = false
@@ -52,6 +55,17 @@ function M.config()
 
     if client.name == "sumneko_lua" then
       client.server_capabilities.documentFormattingProvider = false
+    end
+
+    if client.name == "gopls" and client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format()
+        end,
+      })
     end
 
     if client.name == "omnisharp" then
@@ -154,9 +168,9 @@ function M.config()
   local signs = {
 
     { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
+    { name = "DiagnosticSignWarn",  text = "" },
+    { name = "DiagnosticSignHint",  text = "" },
+    { name = "DiagnosticSignInfo",  text = "" },
   }
 
   for _, sign in ipairs(signs) do
@@ -166,7 +180,7 @@ function M.config()
   local config = {
     virtual_text = true, -- disable virtual text
     signs = {
-      active = signs, -- show signs
+      active = signs,    -- show signs
     },
     update_in_insert = true,
     underline = true,
