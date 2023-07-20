df8bd7ca4771e96857002346be7038e007e2585a
Mathieu Scolas <mathieu.scolas@forem.be>
2023-07-20 09:14:03 +0200

    add fallback

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -30,7 +30,7 @@ function M.config()
     -- keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
     keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
     keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    -- keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    keymap(bufnr, "n", "lgr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
     keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
     keymap(bufnr, "n", "<leader>lI", "<cmd>Mason<cr>", opts)
     -- Keep this code_action keymap waiting few fixes in folke/which-key.nvim
