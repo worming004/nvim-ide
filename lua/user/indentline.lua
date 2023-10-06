1ed17902fd08ea3d443069cf1252eff873c0bd7d
Mathieu Scolas <mathieu.scolas@forem.be>
2023-10-06 08:52:05 +0200

    update indentline

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,14 +1,12 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl"
   event = "BufReadPre",
 }
 
 M.opts = {
   char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
   use_treesitter = true,
-  show_current_context = true,
   buftype_exclude = { "terminal", "nofile" },
   filetype_exclude = {
     "help",
