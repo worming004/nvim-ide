40f55be904f5ed90ac11331fa02220a98c418956
Mathieu Scolas <mathieu.scolas@forem.be>
2023-11-27 13:32:31 +0100

    add commands for tab width

--- lua/commands.lua
+++ lua/commands.lua
@@ -24,3 +24,10 @@ end, opts_with_desc(opts, "Toggle relativenumber"))
 vim.api.nvim_create_user_command("RemoveWindowCr", function(_)
   vim.cmd "%s/\r//g"
 end, opts_with_desc(opts, "Remove all ^M from fil"))
+
+vim.api.nvim_create_user_command("SetTab4", function(_)
+  vim.cmd "set shiftwidth=4"
+end, opts_with_desc(opts, "Set tab to 4"))
+vim.api.nvim_create_user_command("SetTab2", function(_)
+  vim.cmd "set shiftwidth=2"
+end, opts_with_desc(opts, "Set tab to 2"))
