cbf09aa8304e0219032d4a8c1b3d4ace4c317551
Mathieu Scolas <mathieu.scolas@forem.be>
2024-01-24 08:43:15 +0100

    use fix_pairs for copilot

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -32,7 +32,9 @@ local M = {
     {
       "zbirenbaum/copilot-cmp",
       config = function()
-        require("copilot_cmp").setup()
+        require("copilot_cmp").setup({
+          fix_pairs = true,
+        })
       end,
       dependencies = {
         {
