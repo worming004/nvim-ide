35f903638e071bc3f9ac87cfabf6a6a7cd8d7d7a
Mathieu Scolas <worming@hotmail.be>
2024-05-17 17:09:46 +0200

    add options

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,13 @@
 --opts
 vim.opt.clipboard = "unnamedplus"
 vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2     
+vim.opt.tabstop = 2
+vim.opt.fileencoding = "utf-8"
+vim.opt.termguicolors = true
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.ruler = false
+
 vim.g.mapleader = " "
+vim.g.autoformat = true
