81d30a8acad4039bcc6144296c19da5bf08fe3f7
Mathieu Scolas <worming@hotmail.be>
2024-05-17 17:37:45 +0200

    bootstrap

--- init.lua
+++ init.lua
@@ -1,5 +1,17 @@
 require "options"
 require "mylazy"
 require "keymaps"
+require "commands"
+require "autocommands"
+require "open-web"
+require "grenerate-go-tests"
+require "which-key-group"
 
 
+-- open nvimtree at startup
+local fn = vim.fn
+if fn.bufname('%') == '' then
+  if fn.empty(fn.getline(1, '$')) then
+    vim.cmd ":NvimTreeFocus"
+  end
+end
