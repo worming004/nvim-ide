c0fdf405038af83fbaa21b59b02dbc3016aabdea
Mathieu Scolas <worming@hotmail.be>
2024-05-25 16:33:04 +0200

    add comment and remove unused func

--- lua/commands.lua
+++ lua/commands.lua
@@ -5,6 +5,7 @@ local opts_with_desc = function(o, desc)
   return o
 end
 
+-- Autoformat
 vim.api.nvim_create_user_command("AutoformatEnable", function(_)
   vim.g.autoformat = true
 end, opts_with_desc(opts, "Enable autoformat"))
@@ -18,14 +19,17 @@ vim.api.nvim_create_user_command("AutoformatToggle", function(_)
   vim.notify("autoformat : " .. tostring(vim.g.autoformat))
 end, opts_with_desc(opts, "Toggle autoformat"))
 
+-- Visual toggles
 vim.api.nvim_create_user_command("RelativeLineToggle", function(_)
   vim.cmd "set relativenumber!"
 end, opts_with_desc(opts, "Toggle relativenumber"))
 
+-- Windowsh crlf conversion
 vim.api.nvim_create_user_command("RemoveWindowCr", function(_)
   vim.cmd "%s/\r//g"
 end, opts_with_desc(opts, "Remove all ^M from fil"))
 
+-- Set tab width
 vim.api.nvim_create_user_command("SetTab4", function(_)
   vim.cmd "set shiftwidth=4"
 end, opts_with_desc(opts, "Set tab to 4"))
@@ -33,7 +37,7 @@ vim.api.nvim_create_user_command("SetTab2", function(_)
   vim.cmd "set shiftwidth=2"
 end, opts_with_desc(opts, "Set tab to 2"))
 
-local function default_notification(result)
+local function default_notification_from_stdout_stderr(result)
   if not utils.is_null_or_empty(result.stderr) then
     vim.notify(result.stderr, "error")
   end
@@ -42,9 +46,6 @@ local function default_notification(result)
   end
 end
 
-vim.api.nvim_create_user_command("Debug", function(_)
-  vim.notify(tostring(vim.version().minor))
-end, opts)
 vim.api.nvim_create_user_command("KubeApply", function(_)
   if vim.version().minor < 10 then
     vim.notify("using compat version for neovim < 0.10.0", "warn")
@@ -54,7 +55,7 @@ vim.api.nvim_create_user_command("KubeApply", function(_)
   else
     local kubefile = vim.api.nvim_buf_get_lines(0, 0, -1, true)
     local command = { 'kubectl', 'apply', '-f', '-' }
-    vim.system(command, { stdin = kubefile }, default_notification)
+    vim.system(command, { stdin = kubefile }, default_notification_from_stdout_stderr)
   end
 end, opts_with_desc(opts, "Apply current file with kubectl apply -f (be carefull about namespace)"))
 
@@ -67,7 +68,7 @@ vim.api.nvim_create_user_command("KubeDelete", function(_)
   else
     local kubefile = vim.api.nvim_buf_get_lines(0, 0, -1, true)
     local command = { 'kubectl', 'delete', '-f', '-' }
-    vim.system(command, { stdin = kubefile }, default_notification)
+    vim.system(command, { stdin = kubefile }, default_notification_from_stdout_stderr)
   end
 end, opts_with_desc(opts, "Delete current file with kubectl apply -f (be carefull about namespace)"))
 
