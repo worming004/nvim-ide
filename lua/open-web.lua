d35758767edf7496ab7d4d595264d399092b9c4d
Mathieu Scolas <worming@hotmail.be>
2024-05-25 16:59:45 +0200

    add type annotation and clean code

--- lua/open-web.lua
+++ lua/open-web.lua
@@ -1,3 +1,18 @@
+-- This file is shit. It have to be refactored one time
+
+---@alias remote_type
+---| 'github'
+---| 'gitlab'
+---| 'azure'
+---| 'unknown'
+
+---@class open_on_web -- Open on web in one shot. Do not reuse-twice
+---@field remote string -- initial remote address
+---@field current_branch string -- current branch name
+---@field remote_type remote_type -- enum like github, gitlab, azure
+---@field url string -- result, what url to open
+---@field replaced_address string internal field, this is a constantly updated value in order to find what is the address to return
+---@field relative_path string internal field, this is a constantly updated value in order to find what is the path of the file
 local M = {}
 
 local gitutils = require('utils.git')
@@ -8,17 +23,19 @@ local function ends_with(str, ending)
   return ending == "" or str:sub(- #ending) == ending
 end
 
--- This is the main method
+
+-- Bootstrap all the logic in order to discover what is the url heuristicly, and then actually open in browser.
+---@param self table this is the instance of the class
 M.open_current_buffer_on_web = function(self)
   self.remote = trim(vim.fn.system({ 'git', 'remote', 'get-url', 'origin' }))
   self.current_branch = gitutils:get_current_branch_name()
 
   local type = self:detect_remote_type()
   if type[1] ~= 0 then
-    error("cannot detect remote type")
+    error("cannot detect remote remote_type")
   end
 
-  self.type = type[2]
+  self.remote_type = type[2]
 
   self.replaced_address = self:replace_git_format_to_http()
   self.replaced_address = self:trim_git()
@@ -36,7 +53,7 @@ M.detect_remote_type = function(self)
   elseif string.match(self.remote, "azure") then
     return { 0, "azure" }
   else
-    return { 0, "" }
+    return { 0, "unknown" }
   end
 end
 
@@ -52,11 +69,11 @@ M.replace_git_format_to_http = function(self)
     return self.remote
   end
 
-  if self.type == "github" then
+  if self.remote_type == "github" then
     local path = string.sub(self.remote, 16, -5)
     return 'https://github.com/' .. path
   end
-  if self.type == 'azure' then
+  if self.remote_type == 'azure' then
     vim.notify('todo manage azdo and gitlab')
   end
 end
@@ -71,7 +88,7 @@ M.trim_git = function(self)
 end
 
 M.set_relative_path_to_replace_address = function(self)
-  if self.type == "github" then
+  if self.remote_type == "github" then
     return self.replaced_address .. '/blob/' .. self.current_branch .. '/' .. self.relative_path
   end
 end
