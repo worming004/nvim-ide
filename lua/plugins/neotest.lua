eaad7ccc31b2708cac68107c20aee0c50542efd0
Mathieu Scolas <worming@hotmail.be>
2024-06-23 17:14:06 +0200

    use latest neotest-elixir

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-neotest/neotest",
-  event = "VeryLazy",
+  lazy = false,
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
@@ -8,14 +8,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "jfpedroza/neotest-elixir",
     "Issafalcon/neotest-dotnet",
-    "nvim-neotest/neotest-go"
+    "nvim-neotest/neotest-go",
   },
   config = function()
     require("neotest").setup({
       adapters = {
         require("neotest-elixir"),
         require("neotest-dotnet")({
-          discovery_root = "solution"
+          discovery_root = "solution",
+          mix_task = { "test.interactive" }
         }),
         require("neotest-go")({
           recursive_run = true
