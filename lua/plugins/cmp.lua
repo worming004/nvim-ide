4fcac4ea270b664060438e8bd18f1f04496e9bc1
Mathieu Scolas <worming@hotmail.be>
2024-10-25 14:06:59 +0200

    add C-o to trigger complete for mac

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -141,6 +141,7 @@ return {
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<C-e>"] = cmp.mapping {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
