48ba595e0cf433c989bcc10f090b55bcc6c104d0
Mathieu Scolas <mathieu.scolas@forem.be>
2024-12-11 10:25:57 +0100

    better copilot conf

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -62,7 +62,8 @@ return {
             { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
           },
           opts = {
-            debug = true, -- Enable debugging
+            debug = true,      -- Enable debugging
+            context = "files", -- Default context or array of contexts to use (can be specified manually in prompt via #).
             -- See Configuration section for rest
           },
           -- See Commands section for default commands if you want to lazy load on them
