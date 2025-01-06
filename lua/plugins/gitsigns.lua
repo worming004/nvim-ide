167d88ea7d35d301c8fa46e5b553db9eba7d17db
worming <worming@hotmail.be>
2025-01-06 18:59:36 +0100

    add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,32 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "BufReadPre",
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    signs_staged = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+    },
+    auto_attach = true,
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+      use_focus = true,
+    },
+    signs_staged_enable = true,
+  },
+}
