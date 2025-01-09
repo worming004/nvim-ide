ca090af7171e8d69bdf7e751ad5c24b73139911d
Mathieu Scolas <worming@hotmail.be>
2025-01-09 20:49:38 +0100

    add cool stuff

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,7 +2,13 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    -- add any options here
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,   -- requires hrsh7th/nvim-cmp
+      },
+    },
   },
   dependencies = {
     "MunifTanjim/nui.nvim",
