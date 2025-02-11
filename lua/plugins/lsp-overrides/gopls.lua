19bef016e0fe6f2f293f6a84751365c9657f659d
Mathieu Scolas <worming@hotmail.be>
2025-02-11 22:12:01 +0100

    add staticcheck for golang

--- lua/plugins/lsp-overrides/gopls.lua
+++ lua/plugins/lsp-overrides/gopls.lua
@@ -1,15 +1,16 @@
 return {
-	settings = {
-  gopls = {
-    hints = {
-      assignVariableTypes = true,
-      compositeLiteralFields = true,
-      compositeLiteralType = true,
-      constantValues = true,
-      functionTypeParameters = true,
-      parameterNames = true,
-      rangeVariableTypes = true,
-			}
-		}
-	}
+  settings = {
+    gopls = {
+      staticcheck = true,
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralType = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      }
+    }
+  }
 }
