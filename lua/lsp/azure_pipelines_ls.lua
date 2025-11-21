6d67c960ce2763cec1eb1de313c0570034b89064
Mathieu Scolas <mathieu.scolas@forem.be>
2025-11-21 14:29:28 +0100

    use mason install

--- lua/lsp/azure_pipelines_ls.lua
+++ lua/lsp/azure_pipelines_ls.lua
@@ -1,5 +1,5 @@
 vim.lsp.config.azure_pipelines_ls = {
-  cmd = { 'node', '/home/sclmth/git/lsp/azure-pipelines-language-server/language-server/out/server.js', '--stdio' },
+  cmd = { 'azure-pipelines-language-server', '--stdio' },
   filetypes = { 'azure-pipeline.yaml', 'yaml', 'yml' },
   root_markers = { ".git", vim.uv.cwd() },
   settings = {
