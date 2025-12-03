cf05154cdc785e4d7ef7e7052a757e147152c7e1
Mathieu Scolas <mathieu.scolas@forem.be>
2025-12-03 13:26:03 +0100

    format lua_la

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -1,21 +1,21 @@
 vim.lsp.config.lua_ls = {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
-  settings= {
-  Lua= {
-    diagnostics = {
-      globals = { "vim" },
-    },
-  }},
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    }
+  },
   root_markers = {
-  '.luarc.json',
-  '.luarc.jsonc',
-  '.luacheckrc',
-  '.stylua.toml',
-  'stylua.toml',
-  'selene.toml',
-  'selene.yml',
-  '.git',
+    '.luarc.json',
+    '.luarc.jsonc',
+    '.luacheckrc',
+    '.stylua.toml',
+    'stylua.toml',
+    'selene.toml',
+    'selene.yml',
+    '.git',
   },
 }
-
