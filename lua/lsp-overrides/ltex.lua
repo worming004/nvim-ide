5eb3ad8665ac78bc9c0b262aea4a6a7c75ba9982
Mathieu Scolas <mathieu.scolas@forem.be>
2025-12-10 08:12:04 +0100

    revert to previous ltex config

--- lua/lsp-overrides/ltex.lua
+++ lua/lsp-overrides/ltex.lua
@@ -16,6 +16,24 @@ local filetypes = {
   'mail',
   'text',
 }
-return {
-  filetypes = filetypes
+
+local lang = "fr"
+local config = {}
+config = {
+  cmd = { "ltex-ls" },
+  filetypes = filetypes,
+  flags = { debounce_text_changes = 300 },
+  settings = {
+    ltex = {
+      language = lang
+    }
+  },
+  extra_on_attach = function(client, bufnr)
+    require("ltex_extra").setup {
+      init_check = true,
+      load_langs = { lang, "en-US" },
+      path = vim.fn.expand('~') .. '/.local/share/ltex',
+    }
+  end,
 }
+return config
