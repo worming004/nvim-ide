9dd45ac4776be36026bf79ebc6ba190cc71eb712
Mathieu Scolas <mathieu.scolas@forem.be>
2025-12-24 16:56:29 +0100

    debug dotnet project

--- lua/commands.lua
+++ lua/commands.lua
@@ -182,3 +182,65 @@ vim.api.nvim_create_user_command('DotnetBuildQuickFix', function()
   vim.cmd('cexpr system("dotnet build")')
   vim.cmd('copen')
 end, {})
+
+vim.api.nvim_create_user_command('DebugDotnet', function()
+  local function dotnet_build_project()
+    local default_path = vim.fn.getcwd() .. '/'
+    if vim.g['dotnet_last_proj_path'] ~= nil then
+      default_path = vim.g['dotnet_last_proj_path']
+    end
+    local path = vim.fn.input('Path to your *proj file', default_path, 'file')
+    vim.g['dotnet_last_proj_path'] = path
+    local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
+    print('')
+    print('Cmd to execute: ' .. cmd)
+    local f = os.execute(cmd)
+    if f == 0 then
+      print('\nBuild: ✔️ ')
+    else
+      print('\nBuild: ❌ (code: ' .. f .. ')')
+    end
+  end
+  local function get_dotnet_framework(csproj_path)
+    local lines = vim.fn.readfile(csproj_path)
+    local content = table.concat(lines, "\n")
+    local target_framework = content:match("<TargetFramework>(.-)</TargetFramework>")
+    return target_framework
+  end
+
+  local request = function()
+    local buf_path = vim.api.nvim_buf_get_name(0)
+    local dir = vim.fn.fnamemodify(buf_path, ":h")
+    local csproj_files = vim.fn.globpath(dir, "*.csproj", false, true)
+
+    if #csproj_files ~= 1 then
+      return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    else
+      local project_name = csproj_files[1]:match("([^/]+)%.csproj$")
+      local framework = get_dotnet_framework(csproj_files[1])
+
+      local path = vim.fn.input(
+        'Path to dll: ',
+        vim.g['dotnet_last_proj_path'] .. 'bin/Debug/' .. framework .. '/' .. project_name,
+        'file')
+      return path
+    end
+  end
+  if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
+    dotnet_build_project()
+  end
+
+  local dll_path = request()
+  local directory = vim.fn.fnamemodify(dll_path, ":h")
+
+  local dap = require('dap')
+  local dap_config = {
+    name = "Launch - netcoredbg",
+    request = "launch",
+    type = "coreclr",
+    cwd = directory,
+    env = "ASPNETCORE_ENVIRONMENT=Development",
+    program = dll_path
+  }
+  dap.launch(dap.adapters.coreclr, dap_config)
+end, opts_with_desc(opts, "Debug a dotnet project"))
