6e38aedfa0c0219fbcdfe1cdffc565deb16af8a1
Mathieu Scolas <mathieu.scolas@forem.be>
2026-04-10 11:56:42 +0200

    add border color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,13 @@ return {
   name = "catppuccin",
   lazy = false,
   config = function()
+    require("catppuccin").setup({
+      custom_highlights = function(colors)
+        return {
+          WinSeparator = { fg = colors.blue, bold = true },
+        }
+      end,
+    })
     vim.cmd("colorscheme catppuccin")
   end
 }
