d303bffb3117ce9f6e024dc14bf5687c32319202
worming <worming@hotmail.be>
2026-05-03 10:25:27 +0200

    setup transparent background if kitty

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,9 @@ return {
   name = "catppuccin",
   lazy = false,
   config = function()
+    local transparent = vim.env.KITTY_WINDOW_ID ~= nil
     require("catppuccin").setup({
+      transparent_background = transparent,
       custom_highlights = function(colors)
         return {
           WinSeparator = { fg = colors.blue, bold = true },
