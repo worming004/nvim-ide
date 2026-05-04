eacf54bf43038dd4b129ec1c3709e157b99eb3a6
Mathieu Scolas <mathieu.scolas@forem.be>
2026-05-04 13:12:58 +0200

    always setup transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,9 +3,8 @@ return {
   name = "catppuccin",
   lazy = false,
   config = function()
-    local transparent = vim.env.KITTY_WINDOW_ID ~= nil
     require("catppuccin").setup({
-      transparent_background = transparent,
+      transparent_background = true,
       custom_highlights = function(colors)
         return {
           WinSeparator = { fg = colors.blue, bold = true },
