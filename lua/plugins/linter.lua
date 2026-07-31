e0007e348420a2a8aec58ad67b86eda7a3b15254
Mathieu Scolas <mathieu.scolas@forem.be>
2026-07-31 13:46:33 +0200

    rm pyling

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -13,7 +13,7 @@ return {
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       svelte = { "eslint_d" },
-      python = { "pylint" },
+      -- python = { "pylint" },
       -- go = { "golangcilint" },
       zsh = { "zsh" },
     }
